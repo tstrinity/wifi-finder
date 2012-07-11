e172785244c138a0d3076632a088ecd5b50c45fe
ts.trinity <ts.trinity@gmail.com>
2012-07-11 21:35:04 +0300

    some css changes

--- mocca/urls.py
+++ mocca/urls.py
@@ -12,6 +12,6 @@ urlpatterns = patterns('django.views.generic.simple',
     url(r'^providers/', include('providers.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^feedback/', include('feedback.urls')),
-    url(r'^admin_tools/', include('admin_tools.urls')),
+    #url(r'^admin_tools/', include('admin_tools.urls')),
     (r'^$', 'direct_to_template', {'template': 'index.html'}),
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
