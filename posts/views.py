66b2664128495ba9f3b17254f2554be106e38f50
ts.trinity <ts.trinity@gmail.com>
2012-07-12 18:01:40 +0300

    pagination to posts added

--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render_to_response, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from posts.models import Post
 
 def index(request):
     posts = Post.objects.all().order_by('created_at')
+    paginator = Paginator(posts, 2)
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
     return render_to_response('posts/index.html', {'posts' : posts})
 
 def details(request, post_id):
